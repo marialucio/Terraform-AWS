76e6f879a51ed01527abf10cc07660b3a80ee397
Maria Lúcio <dudarina.lucio@gmail.com>
2024-04-08 10:19:03 -0300

    outputs

--- terraform-basico/interpolacao-locals-outputs/s3.tf
+++ terraform-basico/interpolacao-locals-outputs/s3.tf
@@ -11,10 +11,10 @@ resource "aws_s3_bucket_object" "this" {
 
   bucket = aws_s3_bucket.this.bucket
 
-  key    = "config/${local.ip_filepath}"
+  key = "config/${local.ip_filepath}"
 
   source = local.ip_filepath
 
-  etag   = filemd5(local.ip_filepath)
+  etag = filemd5(local.ip_filepath)
 
 }
